2ebb24e45b109eb301db4bb6534ed05fc68d4753
LouiseSloth <louise.sloth@hotmail.com>
2018-05-17 13:42:41 +0200

    fungerende nav til mobil og pc

--- script/script.js
+++ script/script.js
@@ -1,5 +1,12 @@
-//burgermenu ikon
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+$(document).ready(function () {
+
+    $("button .container").click(function () {
+
+        $(this).toggleClass("change"); //får burgermenuen til at skifte til et kryds og omvendt
+        $(".dropdown-content").toggle(); //åbner og lukker dropdownmenu
+
+    });
+
+});
+
 //burgermenu ikon slut
