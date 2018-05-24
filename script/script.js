31a33dc72a3b279f30da3fda22aed121142530f4
LouiseSloth <louise.sloth@hotmail.com>
2018-05-24 14:54:06 +0200

    header, nav mobil er klar, footer mangler redigering, service dias virker, jeg er begyndt på opstilling af indhold.

--- script/script.js
+++ script/script.js
@@ -1,12 +1,43 @@
 $(document).ready(function () {
 
-    $("button .container").click(function () {
+    // nav mobil
+    $("i .container").click(function () {
 
         $(this).toggleClass("change"); //får burgermenuen til at skifte til et kryds og omvendt
         $(".dropdown-content").toggle(); //åbner og lukker dropdownmenu
 
     });
+    // nav mobil slut
 
 });
 
-//burgermenu ikon slut
+//service dias
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+//service dias slut
