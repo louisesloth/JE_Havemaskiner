b2bc83ca4ee9e1aaa84c7ebbf8d79f1d23cdea6a
menja <menja08@yahoo.com>
2018-06-01 21:50:55 +0200

    produkt and produkt_undersides functioning

--- script/script2.js
+++ script/script2.js
@@ -11,6 +11,35 @@ $(document).ready(function () {
 
 });
 
+/* javascript til produkt undersider*/
+
+$(document).ready(function () {
+    $('.tab-panels .tabs li').on('click', function () {
+
+        var panel = $(this).closest('.tab-panels');
+        panel.find('.tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        // figure out which panel to show
+        var panelToShow = $(this).attr('rel');
+
+        // hide current panel
+        panel.find('.panel.active').slideUp(300, showNextPanel);// use hide()
+
+        // show new panel
+        function showNextPanel() {
+            $(this).removeClass('active');
+
+            $('#'+panelToShow).slideDown(300, function() {
+                $(this).addClass('active');
+            });
+
+        }
+    });
+});
+
+/* javascript til dias*/
+
 var myslideIndex1 = 1;
 showSlides1(myslideIndex1);
 
